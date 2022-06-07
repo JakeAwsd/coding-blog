b814142d5a7c283cccdc2e6576dc8b7b4253d32e
JakeAwsd <93892085+JakeAwsd@users.noreply.github.com>
2022-06-06 22:16:54 -0500

    Fixed merge... again

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,16 +1,8 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes.js');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
-=======
-const postRoutes = require('./post-routes.js');
-const userRoutes = require('./user-routes.js');
-
-router.use('/post', postRoutes);
-router.use('/user', userRoutes);
->>>>>>> e936af837b620d2e4579857c06e50e4082032fb0
 
 module.exports = router;

aa80c3a89df2c20227c8d7d758268d1c587085fb
ErinJordan222 <erin.jordan@du.edu>
2022-06-06 19:59:33 -0600

    routes/api

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const matchupRoutes = require('./matchup-routes');
-const techRoutes = require('./tech-routes.js');
+const techRoutes = require('./user-routes.js');
 
 router.use('/matchup', matchupRoutes);
 router.use('/tech', techRoutes);

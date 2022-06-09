218b1a6754280e73902b873d98ecc7c942bd71d7
ErinJordan222 <erin.jordan@du.edu>
2022-06-08 19:46:48 -0600

    login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,61 @@
-function Login(){
-    return (
-        <div>
-            <input type="text" placeholder="username"/>
-            <input type="text" placeholder="password"/>
-            <button>Login</button>
+import React, { useState } from 'react';
+import { From, Button, Alert } from 'react-icons/md';
 
-        </div>
+
+const Login = () => {
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const response = await loginUser(userFormData);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+  };
+    
+    return (
+        <div className="app">
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
+    </div>
+        // <div>
+        //     <input type="text" placeholder="username"/>
+        //     <input type="text" placeholder="password"/>
+        //     <button>Login</button>
+
+        // </div>
     )
 }
 

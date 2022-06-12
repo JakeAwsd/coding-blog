5d275fa3a07ec2e3d479fa3a9002f2540235eeb3
Hugo rubio <hugorubio180@gmail.com>
2022-06-12 10:33:58 -0600

    adde style to hello world pg and a title option to the blog page

--- client/src/pages/resources.js
+++ client/src/pages/resources.js
@@ -1,70 +1,143 @@
-import React from 'react';
-import CommentBox from '../components/comments/comment-box';
-import { Button} from 'react-bootstrap';
+import React from "react";
 
-
-
+import "./resources.css";
 
 export default class ResourcePage extends React.Component {
   render() {
     return (
-        <div>
-          <div className="cell">
-          <article className="article">
-              <h1 className="article-title">Coding Resources</h1>
-              <div className="article-body"></div>
+      <div>
+        <div className="cell">
+          <article className="w3-container">
+            <h1 className="article-title">Coding Resources</h1>
+            <div className="article-body"></div>
 
-              {/* sites to help with coding errors, etc */}
-              <p>
-                Stack Overflow- Question and Answer Website for Professional and Enthusiast Programmers!
-              </p>
-              <Button href="#">https://stackoverflow.com/</Button> <Button type="submit">View More</Button>{' '}
-              <p>
-                MDN Web Docs- Documentation Repository and Learning Resource for Web Developers.
-              </p>
-              <Button href="#">https://mdn.dev/</Button> <Button type="submit">View More</Button>{' '}
-              <p>
-                Code Project- Community for Computer Programmers w/ Articles on Different Topics and Programming Languages.
-              </p>
-              <Button href="#">https://www.codeproject.com/</Button> <Button type="submit">View More</Button>{' '}
-              <p>
-                Code Ranch- Discussions on Java, Databases, Programmer Certification, Programming Jobs and More.
-              </p>
-              <Button href="#">https://coderanch.com/</Button> <Button type="submit">View More</Button>{' '}
+            {/* sites to help with coding errors, etc */}
 
-              {/* resource pages to help w/ coding */}
+            <div className="w3-teal">
               <p>
-                CodeAcademy- Online Interactive Platform offering Free Coding Classes
+                Stack Overflow- Question and Answer Website for Professional and
+                Enthusiast Programmers!
+                <a className="btn"
+                  href="https://stackoverflow.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
               </p>
-              <Button href="#">https://www.codecademy.com/</Button> <Button type="submit">View More</Button>{' '}
-              <p>
-                GeeksforGeeks- Tutorial Coding Site including Quizzes and Algorithm
-              </p>
-              <Button href="#">https://www.geeksforgeeks.org/</Button> <Button type="submit">View More</Button>{' '}
-              <p>
-                W3Schools- Freemium Educational Website for Learning Coding Online
-              </p>
-              <Button href="#">https://www.w3schools.com/</Button> <Button type="submit">View More</Button>{' '}
-              {/* front end websites to help design your page */}
-              <p>
-                CodePen- Build, Test, and Discover Front End Code
-              </p>
-              <Button href="#">https://codepen.io/</Button> <Button type="submit">View More</Button>{' '}
-              <p>
-                Coolors- Color Palattes Generator    
-              </p>
-              <Button href="#">https://coolors.co/</Button> <Button type="submit">View More</Button>{' '}
-              <p>
-                FontSpace- 93,000+ Free Fonts
-              </p>
-              <Button href="#">fontspace.com</Button> <Button type="submit">View More</Button>{' '}
-              
-              </article>
-          </div>
+            </div>
+            <p>
+              MDN Web Docs- Documentation Repository and Learning Resource for
+              Web Developers.
+              <a className="btn"
+                  href="https://mdn.dev"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
+            </p>
 
-          {/* Load the component CommentBox with the required prop apiUrl */}
-          <CommentBox apiUrl="api/resources/comments.json" />
+            <p>
+              Code Project- Community for Computer Programmers w/ Articles on
+              Different Topics and Programming Languages.
+        
+              <a className="btn"
+                  href="https://www.codeproject.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
+
+
+            </p>
+            <p>
+              Code Ranch- Discussions on Java, Databases, Programmer
+              Certification, Programming Jobs and More.
+   
+              <a  className="btn"
+                  href="https://coderanch.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
+            </p>
+
+            {/* resource pages to help w/ coding */}
+            <p>
+              CodeAcademy- Online Interactive Platform offering Free Coding
+              Classes
+             
+              <a  className="btn"
+                  href="https://www.codecademy.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
+            </p>
+            <p>
+              GeeksforGeeks- Tutorial Coding Site including Quizzes and
+              Algorithm
+        
+              <a  className="btn"
+                  href="https://www.geeksforgeeks.org/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
+            </p>
+            <p>
+              W3Schools- Freemium Educational Website for Learning Coding Online
+            
+              <a  className="btn"
+                  href="https://www.w3schools.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
+            </p>
+            {/* front end websites to help design your page */}
+            <p>
+              CodePen- Build, Test, and Discover Front End Code
+           
+              <a  className="btn"
+                  href="https://codepen.io/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
+            </p>
+            <p>
+              Coolors- Color Palattes Generator
+           
+              <a  className="btn"
+                  href="https://coolors.co/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
+            </p>
+            <p>
+              FontSpace- 93,000+ Free Fonts
+       
+              <a  className="btn"
+                  href="https://www.fontspace.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn">view More</button>
+                </a>
+            </p>
+          </article>
         </div>
-    )
+      </div>
+    );
   }
 }
